7dd6aa0f22d6787be905435f385ee799e7ff2e78
bafplus <bart.cockheyt@gmail.com>
2025-09-30 22:54:33 +0200

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -7,7 +7,7 @@ export $(grep -v '^#' /var/www/html/.env | xargs)
 echo "Waiting for MariaDB at $DB_HOST..."
 
 # Wait until the database container is ready
-until mysqladmin ping -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" --silent --ssl-mode=DISABLED; do
+until mysqladmin ping -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" --silent --skip-ssl; do
   sleep 2
 done
 
@@ -15,15 +15,15 @@ echo "MariaDB is ready."
 
 # Load schema if database is empty
 if [ -f /var/www/html/database.sql ]; then
-    TABLES=$(mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" --ssl-mode=DISABLED -e "SHOW TABLES;" | wc -l)
+    TABLES=$(mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" --skip-ssl -e "SHOW TABLES;" | wc -l)
     if [ "$TABLES" -le 0 ]; then
         echo "Loading database schema..."
-        mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" --ssl-mode=DISABLED < /var/www/html/database.sql
+        mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" --skip-ssl < /var/www/html/database.sql
     else
         echo "Database already has tables, skipping schema import."
     fi
 fi
 
-# Start Supervisor (runs Apache + other services)
+# Start Supervisor (runs Apache + any other services)
 exec /usr/bin/supervisord -n
 
