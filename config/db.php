e656c8ccc98d497610ac6d8b482b9df89ffb8238
bafplus <bart.cockheyt@gmail.com>
2025-09-30 19:59:10 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php'; // adjust path if needed
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
